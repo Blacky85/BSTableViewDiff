5f236a8589dd11e8f386b368695f26c8edbfc99a
Michael Schwarz <michael_schwarz@me.com>
2015-02-14 16:59:00 +0100

    improve example project

--- BSTableViewDiff/BSTableViewDiff/ViewController.m
+++ BSTableViewDiff/BSTableViewDiff/ViewController.m
@@ -14,6 +14,8 @@
 
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (strong, nonatomic) BSTableViewModel *tableViewModel;
+@property (weak, nonatomic) IBOutlet UIScrollView *selectionButtonsScrollview;
+@property (nonatomic, strong) NSArray *dataSets;
 
 @end
 
@@ -25,20 +27,13 @@
     self.tableViewModel = [BSTableViewModel tableViewModelWithSectionTitleBlock:^NSString *(id object) {
         return [object valueForKey:@"key"];
     }];
+    [self setupDataSets];
 }
 
-- (void)viewDidAppear:(BOOL)animated {
-    [super viewDidAppear:animated];
-    NSArray *objects = @[@{@"key" : @"section 0",@"hash": @"01"},
-                         @{@"key" : @"section 0",@"hash": @"02"},
-                         @{@"key" : @"section 0.5",@"hash": @"11"},
-                         @{@"key" : @"section 0.5",@"hash": @"12"},
-                         @{@"key" : @"section 1",@"hash": @"21"},
-                         @{@"key" : @"section 1",@"hash": @"22"},
-                         @{@"key" : @"section 2",@"hash": @"31"},
-                         @{@"key" : @"section 2",@"hash": @"32"}];
-    BSTableViewModelDiffSet *diffset = [self.tableViewModel diffSetForDataArray:objects];
-    [self.tableView applyDiffSet:diffset];
+#pragma mark - target/action 
+
+- (void)btnPressed:(UIButton *)btn {
+    [self selectDataSet:btn.tag];
 }
 
 #pragma mark - uitableview delegate
@@ -60,6 +55,52 @@
 
 - (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
     return [self.tableViewModel titleForSection:section];
+}
+
+#pragma mark - private
+
+- (void)selectDataSet:(NSUInteger)dataSetNumber {
+    NSArray *dataSet = self.dataSets[dataSetNumber];
+    BSTableViewModelDiffSet *diffset = [self.tableViewModel diffSetForDataArray:dataSet];
+    [self.tableView applyDiffSet:diffset];
+}
+
+- (void)setupDataSets {
+    NSArray *objects0 = @[@{@"key" : @"section 0",@"hash": @"01"},
+                         @{@"key" : @"section 0",@"hash": @"02"},
+                         @{@"key" : @"section 0.5",@"hash": @"11"},
+                         @{@"key" : @"section 0.5",@"hash": @"12"},
+                         @{@"key" : @"section 1",@"hash": @"21"},
+                         @{@"key" : @"section 1",@"hash": @"22"},
+                         @{@"key" : @"section 2",@"hash": @"31"},
+                         @{@"key" : @"section 2",@"hash": @"32"}];
+    
+    NSArray *objects1 = @[@{@"key" : @"section 0",@"hash": @"02"},
+                         @{@"key" : @"section 0.5",@"hash": @"13"},
+                         @{@"key" : @"section 0.5",@"hash": @"12"},
+                         @{@"key" : @"section 1",@"hash": @"21"},
+                         @{@"key" : @"section 2",@"hash": @"24"},
+                         @{@"key" : @"section 2",@"hash": @"31"},
+                         @{@"key" : @"section 2",@"hash": @"32"}];
+    
+    self.dataSets = @[objects0,objects1];
+    
+    self.selectionButtonsScrollview.pagingEnabled = YES;
+    
+    CGFloat width = self.view.frame.size.width;
+    CGFloat height = self.selectionButtonsScrollview.frame.size.height;
+    for (NSUInteger btnNumber = 0; btnNumber < self.dataSets.count; btnNumber++) {
+        UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+        btn.frame = CGRectMake(width * btnNumber, 0.f, width, height);
+        [btn setTitle:[NSString stringWithFormat:@"Dataset Number: %zd",btnNumber]forState:UIControlStateNormal];
+        [btn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        btn.tag = btnNumber;
+        [self.selectionButtonsScrollview addSubview:btn];
+        [btn addTarget:self action:@selector(btnPressed:) forControlEvents:UIControlEventTouchUpInside];
+        self.selectionButtonsScrollview.contentSize = CGSizeMake((btnNumber + 1) * width, height);
+    }
+    
+    [self selectDataSet:0];
 }
 
 @end
