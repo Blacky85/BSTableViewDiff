b1560d80ca202472c8c64b32999339cba8b8d0b9
Michael Schwarz <michael_schwarz@me.com>
2015-02-14 12:25:36 +0100

    adds empty project

---
+++ BSTableViewDiff/BSTableViewDiff/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BSTableViewDiff
+//
+//  Created by Michael Schwarz on 14.02.15.
+//  Copyright (c) 2015 Blacksoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BSTableViewDiff/BSTableViewDiff/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BSTableViewDiff
+//
+//  Created by Michael Schwarz on 14.02.15.
+//  Copyright (c) 2015 Blacksoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
